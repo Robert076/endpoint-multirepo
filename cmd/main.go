15b38a0ba55fdb00c7b09e15c92f146bfc2be675
Robert076 <robertberes06@gmail.com>
2025-06-11 18:18:07 +0300

    env file handling

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -11,7 +14,16 @@ func main() {
 	http.HandleFunc("/endpoint", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(w, "Only POST requests allowed to this endpoint.", http.StatusMethodNotAllowed)
-			log.Print("%s: Only POST requests allowed to this endpoint", serviceName)
+			log.Printf("%s: Only POST requests allowed to this endpoint", serviceName)
 		}
+
+		if err := godotenv.Load(); err != nil {
+			log.Fatalf("%s: Cannot load env file", serviceName)
+			return
+		}
+		validatorHost := os.Getenv("VALIDATOR_HOST")
+		validatorPort := os.Getenv("VALIDATOR_PORT")
+
+		http.Get(validatorHost + ":" + validatorPort)
 	})
 }
