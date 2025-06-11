6ac4e9c3c870069bd3ac3a990a73beac85977689
Robert076 <robertberes06@gmail.com>
2025-06-11 17:55:21 +0300

    method checking

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	var serviceName string = "ENDPOINT"
+
+	http.HandleFunc("/endpoint", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Only POST requests allowed to this endpoint.", http.StatusMethodNotAllowed)
+			log.Print("%s: Only POST requests allowed to this endpoint", serviceName)
+		}
+	})
 }
