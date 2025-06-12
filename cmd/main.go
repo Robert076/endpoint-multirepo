4d8bcbcf869deb603ac868f4630e3262b55d7a2c
Robert076 <robertberes06@gmail.com>
2025-06-12 11:44:53 +0300

    main logic + data structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/Robert076/endpoint-multirepo/internal/data"
 	"github.com/joho/godotenv"
 )
 
@@ -26,6 +29,34 @@ func main() {
 		validatorHost := os.Getenv("VALIDATOR_HOST")
 		validatorPort := os.Getenv("VALIDATOR_PORT")
 
-		http.Get("http://" + validatorHost + ":" + validatorPort)
+		req := new(data.RequestBody)
+		req.Name = "test"
+
+		b, err := json.Marshal(req)
+
+		if err != nil {
+			http.Error(w, "Cannot serialize JSON from native Go struct type", http.StatusInternalServerError)
+			log.Printf("%s: Cannot serialize json from native Go struct type %v", serviceName, err)
+		}
+
+		body := bytes.NewBuffer(b)
+
+		response, err := http.Post("http://"+validatorHost+":"+validatorPort, "application/json; charset=utf-8", body)
+
+		if err != nil {
+			http.Error(w, "Cannot validate request.", http.StatusInternalServerError)
+			log.Printf("%s: Cannot validate request. Validator endpoint unreachable. Error: %v", serviceName, err)
+			return
+		}
+
+		defer response.Body.Close()
+
+		log.Printf("Status received from server is: %s", response.Status)
+		log.Printf("StatusCode received from server is: %d", response.StatusCode)
 	})
+
+	endpointPort := os.Getenv("endpointPort")
+	if err := http.ListenAndServe(":"+endpointPort, nil); err != nil {
+		log.Fatalf("%s: Error starting http server, %v", serviceName, err)
+	}
 }
---
+++ internal/data/data.go
@@ -0,0 +1,5 @@
+package data
+
+type RequestBody struct {
+	Name string `json:"name"`
+}
